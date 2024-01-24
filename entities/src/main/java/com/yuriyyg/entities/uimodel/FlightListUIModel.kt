e937b24056410c35f7b46825bb8296703ba9061a
amilt <kukunovyura29@gmail.com>
2024-01-25 02:09:53 +0400

    UseCase+Flow+mapper

---
+++ entities/src/main/java/com/yuriyyg/entities/uimodel/FlightListUIModel.kt
@@ -0,0 +1,14 @@
+package com.yuriyyg.entities.uimodel
+
+data class FlightSearchUIState (
+    val header: SearchHeaderUiModel,
+    val flights: List<FlightListUIModel>)
+
+data class SearchHeaderUiModel(
+    val origin : String,
+    val destination: String)
+
+data class FlightListUIModel(
+    var airlineIcon : String,
+    var airlineName : String,
+    val price : Double )
